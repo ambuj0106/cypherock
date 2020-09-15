451e6eb99d51661d175033979360b2138621abe2
ambuj0106 <46368167+ambuj0106@users.noreply.github.com>
2020-09-15 16:12:09 +0530

    Update main.c

--- task2/main.c
+++ task2/main.c
@@ -2,10 +2,10 @@
 #include "crypto/bip39.h"
 int main()
 {
- char mnemonic[2000];
- scanf("%s",&mnemonic); 
+ char mnemonic[]="garden reject beauty inch scissors rifle amazing couch bacon multiply swim poverty impose spray ugly term stamp prevent nothing mutual awful project wrist movie";
+ 
  uint8_t entropy[32];
-int ent= mnemonic_to_entropy(mnemonic,entropy);
+ mnemonic_to_entropy(mnemonic,entropy);
 for(int i=0;i<32;i++)
 {
 printf("%x ",entropy[i]);
