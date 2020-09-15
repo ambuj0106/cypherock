ed32122045f8d267a3d8cb8d8aae320993f19a22
ambuj0106 <46368167+ambuj0106@users.noreply.github.com>
2020-09-15 17:46:07 +0530

    Update main.c

--- task2/main.c
+++ task2/main.c
@@ -5,11 +5,8 @@ int main()
  char mnemonic[]="garden reject beauty inch scissors rifle amazing couch bacon multiply swim poverty impose spray ugly term stamp prevent nothing mutual awful project wrist movie";
  
  uint8_t entropy[32];
- mnemonic_to_entropy(mnemonic,entropy);
-for(int i=0;i<32;i++)
-{
-printf("%x ",entropy[i]);
-}
+int ent= mnemonic_to_entropy(mnemonic,entropy);
+printf("%d",ent);
 	return 0;
 
 }
