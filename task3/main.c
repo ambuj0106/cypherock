000d6ab7e843a2fa9578a507c8ba97af7af481f7
ambuj0106 <46368167+ambuj0106@users.noreply.github.com>
2020-09-16 20:27:34 +0530

    Update main.c

--- task3/main.c
+++ task3/main.c
@@ -9,14 +9,19 @@ int main()
    
     
      HDNode *master_node=(HDNode *)malloc(sizeof(HDNode));
-      hdnode_from_seed(seed,64, "secp256k1",master_node);
+      
+      mnemonic_to_seed(c, passphrase,seed,(current,total));
+
+      hdnode_from_seed(seed,64,"secp256k1",master_node);
       
     
       //filling public key
        hdnode_fill_public_key(master_node);
     
+
     //master node-> purpose node
 	int pn=hdnode_private_ckd(master_node, (uint32_t)44); 
+	//printf("%d\n",master_node->depth);
    //purpose node-> coin node	
    int cn=hdnode_private_ckd(master_node, (uint32_t)1);
    //coin node-> account  node
@@ -24,6 +29,7 @@ int main()
    //acc node -> chain node
    int chainn=hdnode_private_ckd(master_node, (uint32_t)0);
    //chain node-> address node
+   //printf("%d\n",master_node->depth);
    
    int addn=hdnode_private_ckd(master_node, (uint32_t)0);
 // filling address node public key 
@@ -32,7 +38,7 @@ int main()
    //printing public key
    for(int i=0;i<32;i++)
    {
-   	printf("%x",master_node->public_key[i]);
+   	printf("%x",master_node->private_key[i]);
    }
     printf("\n");
     
