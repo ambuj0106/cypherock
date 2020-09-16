e81d0ac1dd47da349f7b084927642a4e9e658563
ambuj0106 <46368167+ambuj0106@users.noreply.github.com>
2020-09-16 19:58:35 +0530

    Add files via upload

---
+++ task3/main.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include "bip39.h"
+#include "bip32.h"
+int main()
+{
+	const char passphrase[]="",c[]="client power maple tonight gossip apart kite seed rebuild museum ankle cheese coil note property account aware monkey spread brief follow normal unit inside";
+    uint8_t seed[64];
+    uint32_t current=0,total=0;
+   
+    
+     HDNode *master_node=(HDNode *)malloc(sizeof(HDNode));
+      hdnode_from_seed(seed,64, "secp256k1",master_node);
+      
+    
+      //filling public key
+       hdnode_fill_public_key(master_node);
+    
+    //master node-> purpose node
+	int pn=hdnode_private_ckd(master_node, (uint32_t)44); 
+   //purpose node-> coin node	
+   int cn=hdnode_private_ckd(master_node, (uint32_t)1);
+   //coin node-> account  node
+   int accn=hdnode_private_ckd(master_node, (uint32_t)0);
+   //acc node -> chain node
+   int chainn=hdnode_private_ckd(master_node, (uint32_t)0);
+   //chain node-> address node
+   
+   int addn=hdnode_private_ckd(master_node, (uint32_t)0);
+// filling address node public key 
+   hdnode_fill_public_key(master_node);
+   
+   //printing public key
+   for(int i=0;i<32;i++)
+   {
+   	printf("%x",master_node->public_key[i]);
+   }
+    printf("\n");
+    
+
+    
+     
+	return 0;
+}
